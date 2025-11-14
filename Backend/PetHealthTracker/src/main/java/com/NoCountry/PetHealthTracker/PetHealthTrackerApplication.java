636e9375c6793a9560b98d5222aa341c3785ccd5
Solus <gilgamesh06lg@gmail.com>
2025-11-13 19:13:05 -0500

    Proyecto Inicial Backend

---
+++ Backend/PetHealthTracker/src/main/java/com/NoCountry/PetHealthTracker/PetHealthTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.NoCountry.PetHealthTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHealthTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetHealthTrackerApplication.class, args);
+	}
+
+}
---
+++ Backend/PetHealthTracker/src/test/java/com/NoCountry/PetHealthTracker/PetHealthTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NoCountry.PetHealthTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHealthTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
