94d8c4b9673416e9ea1e90af8a6283f25a4256c5
Solus <gilgamesh06lg@gmail.com>
2025-11-15 08:28:55 -0500

    test: Se actualizo el script que inserta data, para probar el diseño de la db.

--- DataBase/init-scripts/insert.sql
+++ DataBase/init-scripts/insert.sql
@@ -1,50 +1,75 @@
+
 -- Entidad: Persona
-INSERT INTO persona (nombre, apellido, fecha_nacimiento) VALUES
-('Carlos', 'Gómez', '1990-05-12'),
-('María', 'López', '1985-09-22'),
-('Juan', 'Martínez', '1998-02-11');
+INSERT INTO persona (nombre, apellido, fecha_nacimiento, fecha_creacion,
+                     fecha_actualizacion, fecha_eliminacion, estado) VALUES
+('Carlos', 'Gómez', '1990-05-12', NOW(), NULL, NULL, 'ACTIVO'),
+('María', 'López', '1985-09-22', NOW(), NULL, NULL, 'ACTIVO'),
+('Juan', 'Martínez', '1998-02-11', NOW(), NULL, NULL, 'ACTIVO');
+
 
 -- Entidad: Usuario
-INSERT INTO usuario (email, password, persona_id) VALUES
-('carlos@gmail.com', 'pass123', 1),
-('maria@gmail.com', 'pass456', 2),
-('juan@gmail.com', 'pass789', 3);
+INSERT INTO usuario (email, password, persona_id, fecha_creacion, fecha_actualizacion,
+                     fecha_eliminacion, estado) VALUES
+('carlos@gmail.com', 'pass123', 1, NOW(), NULL, NULL, 'ACTIVO'),
+('maria@gmail.com', 'pass456', 2, NOW(), NULL, NULL, 'ACTIVO'),
+('juan@gmail.com', 'pass789', 3, NOW(), NULL, NULL, 'ACTIVO');
 
 -- Entidad: Mascota
-INSERT INTO mascota (nombre, especie, raza, fecha_nacimiento, peso, foto, usuario_id) VALUES
-('Firulais', 'Perro', 'Labrador', '2020-03-10', 25.40, 'firulais.jpg', 1),
-('Mishi', 'Gato', 'Angora', '2019-07-15', 4.30, 'mishi.png', 2),
-('Rex', 'Perro', 'Pastor Alemán', '2021-01-05', 30.10, NULL, 1);
+INSERT INTO mascota (nombre, especie, raza, fecha_nacimiento, peso, foto, usuario_id,
+                     fecha_creacion, fecha_actualizacion, fecha_eliminacion, estado) VALUES
+('Firulais', 'Perro', 'Labrador', '2020-03-10', 25.40, 'firulais.jpg', 1, NOW(), NULL, NULL, 'ACTIVO'),
+('Mishi', 'Gato', 'Angora', '2019-07-15', 4.30, 'mishi.png', 2, NOW(), NULL, NULL, 'ACTIVO'),
+('Rex', 'Perro', 'Pastor Alemán', '2021-01-05', 30.10, NULL, 1, NOW(), NULL, NULL, 'ACTIVO');
+
 
 -- Entidad: Comida
-INSERT INTO comida (nombre, descripcion) VALUES
-('Dog Chow', 'Alimento balanceado para perro adulto.'),
-('Cat Premium', 'Alimento premium para gatos adultos.'),
-('Dog Puppy', 'Alimento para cachorros en crecimiento.');
+INSERT INTO comida (nombre, descripcion, fecha_creacion,
+                    fecha_actualizacion, fecha_eliminacion, estado) VALUES
+('Dog Chow', 'Alimento balanceado para perro adulto.', NOW(), NULL, NULL, 'ACTIVO'),
+('Cat Premium', 'Alimento premium para gatos adultos.', NOW(), NULL, NULL, 'ACTIVO'),
+('Dog Puppy', 'Alimento para cachorros en crecimiento.', NOW(), NULL, NULL, 'ACTIVO');
+
 
 -- Entidad: Horario
-INSERT INTO horario (hora, cantidad, dia_semana, recordatorio, mascota_id, comida_id) VALUES
-('08:00', 200, 1, true, 1, 1),  -- Firulais lunes
-('18:00', 200, 1, true, 1, 1),  -- Firulais lunes tarde
-('09:00', 100, 3, true, 2, 2),  -- Mishi miércoles
-('07:30', 250, 5, false, 3, 3); -- Rex viernes
+INSERT INTO horario (hora, cantidad, dia_semana, recordatorio, mascota_id, comida_id,
+                     fecha_creacion, fecha_actualizacion, fecha_eliminacion, estado) VALUES
+('08:00', 200, 1, true, 1, 1, NOW(), NULL, NULL, 'ACTIVO'),
+('18:00', 200, 1, true, 1, 1, NOW(), NULL, NULL, 'ACTIVO'),
+('09:00', 100, 3, true, 2, 2, NOW(), NULL, NULL, 'ACTIVO'),
+('07:30', 250, 5, false, 3, 3, NOW(), NULL, NULL, 'ACTIVO');
+
 
 -- Entidad: Medicamento 
-INSERT INTO medicamento (nombre, tipo, fabricante, intervalo_dosis, descripcion) VALUES
-('RabiaVac', 'VACUNA', 'VetLabs', 365, 'Vacuna anual contra la rabia.'),
-('ParasiClean', 'DESPARASITANTE', 'AnimalPharma', 90, 'Elimina parásitos internos.'),
-('PoliVac', 'VACUNA', 'PetHealth', 180, 'Vacuna polivalente para perros.');
+INSERT INTO medicamento (nombre, tipo, fabricante, intervalo_dosis, descripcion,
+                         fecha_creacion, fecha_actualizacion, fecha_eliminacion, estado) VALUES
+('RabiaVac', 'VACUNA', 'VetLabs', 365, 'Vacuna anual contra la rabia.', NOW(), NULL, NULL, 'ACTIVO'),
+('ParasiClean', 'DESPARASITANTE', 'AnimalPharma', 90, 'Elimina parásitos internos.', NOW(), NULL, NULL, 'ACTIVO'),
+('PoliVac', 'VACUNA', 'PetHealth', 180, 'Vacuna polivalente para perros.', NOW(), NULL, NULL, 'ACTIVO');
+
 
 -- Entidad: Evento Salud
-INSERT INTO evento_salud (tipo, fecha_realizacion, fecha_programada, estado, notas, veterinario, mascota_id, medicamento_id)VALUES
-('VACUNACION', '2024-01-10', '2025-01-10', 'REALIZADO', 'Aplicada sin novedad', 'Dr. Ramírez', 1, 1),
-('DESPARACITACION', '2024-02-15', '2024-05-15', 'REALIZADO', 'Buena respuesta', 'Dra. Muñoz', 1, 2),
-('VISITA', NULL, '2024-11-01', 'PROGRAMADO', 'Control general', 'Dr. López', 2, NULL),
-('VACUNACION', NULL, '2024-12-12', 'PROGRAMADO', 'Vacuna polivalente', 'Dra. González', 3, 3);
+INSERT INTO evento_salud (tipo, fecha_realizacion, fecha_programada, estado_evento, notas, veterinario,
+                          mascota_id, medicamento_id,
+                          fecha_creacion, fecha_actualizacion, fecha_eliminacion, estado) VALUES
+('VACUNACION', '2024-01-10', '2025-01-10', 'REALIZADO', 'Aplicada sin novedad', 'Dr. Ramírez',
+ 1, 1, NOW(), NULL, NULL, 'ACTIVO'),
+('DESPARACITACION', '2024-02-15', '2024-05-15', 'REALIZADO', 'Buena respuesta', 'Dra. Muñoz',
+ 1, 2, NOW(), NULL, NULL, 'ACTIVO'),
+('VISITA', NULL, '2024-11-01', 'PROGRAMADO', 'Control general', 'Dr. López',
+ 2, NULL, NOW(), NULL, NULL, 'ACTIVO'),
+('VACUNACION', NULL, '2024-12-12', 'PROGRAMADO', 'Vacuna polivalente', 'Dra. González',
+ 3, 3, NOW(), NULL, NULL, 'ACTIVO');
+
 
 -- Entidad: Recordatorio
-INSERT INTO recordatorio (fecha_recordatorio, canal, estado, mensaje, evento_salud_id, horario_id, usuario_id) VALUES
-('2024-12-11 08:00:00', 'EMAIL', 'PENDIENTE', 'Recordatorio: vacuna de Rex mañana.', 4, NULL, 1),
-('2024-11-01 07:00:00', 'IN-APP', 'PENDIENTE', 'Tienes una cita veterinaria programada.', 3, NULL, 2),
-('2024-10-02 07:30:00', 'EMAIL', 'ENVIADO', 'Hora de alimentar a Firulais.', NULL, 1, 1),
-('2024-10-02 18:00:00', 'IN-APP', 'PENDIENTE', 'Alimentación programada para Firulais.', NULL, 2, 1);
+INSERT INTO recordatorio (fecha_recordatorio, canal, estado_recordatorio, mensaje,
+                          evento_salud_id, horario_id, usuario_id,
+                          fecha_creacion, fecha_actualizacion, fecha_eliminacion, estado) VALUES
+('2024-12-11 08:00:00', 'EMAIL', 'PENDIENTE', 'Recordatorio: vacuna de Rex mañana.',
+ 4, NULL, 1, NOW(), NULL, NULL, 'ACTIVO'),
+('2024-11-01 07:00:00', 'IN-APP', 'PENDIENTE', 'Tienes una cita veterinaria programada.',
+ 3, NULL, 2, NOW(), NULL, NULL, 'ACTIVO'),
+('2024-10-02 07:30:00', 'EMAIL', 'ENVIADO', 'Hora de alimentar a Firulais.',
+ NULL, 1, 1, NOW(), NULL, NULL, 'ACTIVO'),
+('2024-10-02 18:00:00', 'IN-APP', 'PENDIENTE', 'Alimentación programada para Firulais.',
+ NULL, 2, 1, NOW(), NULL, NULL, 'ACTIVO');
