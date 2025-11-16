59f4dfcf827b978a9405ecc34235938fe37b0909
Solus <gilgamesh06lg@gmail.com>
2025-11-16 13:55:44 -0500

    feat: Se creo la clase Persona que modela la entidad persona en la db.

--- Backend/PetHealthTracker/src/main/java/com/NoCountry/PetHealthTracker/model/entity/Persona.java
+++ Backend/PetHealthTracker/src/main/java/com/NoCountry/PetHealthTracker/model/entity/Persona.java
@@ -1,4 +1,30 @@
 package com.NoCountry.PetHealthTracker.model.entity;
 
-public class Persona {
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode( callSuper = false, onlyExplicitlyIncluded = true)
+@Entity(name = "persona")
+public class Persona extends AuditoriaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    private String apellido;
+
+    @Column(name = "fecha_nacimiento", nullable = false )
+    private LocalDate fechaNacimiento;
+
 }
