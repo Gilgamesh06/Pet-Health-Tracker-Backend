609e74923b945aa379747ab68d318615b7335691
Solus <gilgamesh06lg@gmail.com>
2025-11-16 14:04:20 -0500

    feat: Se creo la clase SecurityConfig que contiene la configuracion de Seguridad y Cadena de filtros.

--- Backend/PetHealthTracker/src/main/java/com/NoCountry/PetHealthTracker/config/SecurityConfig.java
+++ Backend/PetHealthTracker/src/main/java/com/NoCountry/PetHealthTracker/config/SecurityConfig.java
@@ -1,4 +1,93 @@
 package com.NoCountry.PetHealthTracker.config;
 
+import com.NoCountry.PetHealthTracker.config.filters.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * Clase que define la seguridad de mi app {endpoints publicos y privados}, metodo de autenticacion
+ */
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    /**
+     * Consturctor que me instacias las dependencias necesarias
+     * @param userDetailsService  clase que instancia de la interfaz UserDetailService que contiene el metodo que retorna un UserDetails
+     * @param passwordEncoder metodo definido como un bean que encripta y desecritas la contraseña
+     * @param jwtAuthenticationFilter Clase que contiene el metodo que verifica si la ruta es publica o priivada y si es privada verifica
+     *                                que el usuario este en el contexto de autenticacion de spring si no lo guarda en este
+     */
+    public SecurityConfig(UserDetailsService userDetailsService,
+                          PasswordEncoder passwordEncoder,
+                          JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+    /**
+     * Metoddo que genera el provider por medio del UserDetailsService {objeto UserDetails} y
+     * el passwordEncoder
+     * @return retorna un objeto de la clase AunthenticationProvider
+     */
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
+    }
+
+    /**
+     * Metodo que re
+     * @param config
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    /**
+     * Metodo que genera la cadena de filtros de Seguridad
+     * @param http http que recibe
+     * @return retorna una cadena de filtros
+     * @throws Exception
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable()) // Deshabilitar CSRF para APIs stateless
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll() // Endpoints públicos
+                        .anyRequest().authenticated() // Cualquier otra petición requiere autenticación
+                )
+                // Configura la gestión de sesión como STATELESS
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                // Pre_autentica con el filtro defino jwtAuthenticationFitler antes que lo haga el filtro por defecto de formulario
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 }
