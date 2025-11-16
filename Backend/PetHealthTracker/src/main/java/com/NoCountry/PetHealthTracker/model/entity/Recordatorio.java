bc1ba3b4c461835bcf62b95e0692807377b198f6
Solus <gilgamesh06lg@gmail.com>
2025-11-16 13:54:35 -0500

    feat: Se creo la clase Recordatorio que modela la entidad recordatorio en la db.

---
+++ Backend/PetHealthTracker/src/main/java/com/NoCountry/PetHealthTracker/model/entity/Recordatorio.java
@@ -0,0 +1,45 @@
+package com.NoCountry.PetHealthTracker.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false, onlyExplicitlyIncluded = true)
+@Entity(name = "recordatorio")
+public class Recordatorio extends AuditoriaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "fecha_recordatorio", nullable = false)
+    private LocalDateTime fechaRecordatorio;
+
+    @Column(nullable = false)
+    private String canal;
+
+    @Column(name = "estado_recordatorio", nullable = false)
+    private String estadoRecordatorio;
+
+    @Column(nullable = false)
+    private String mensaje;
+
+    @ManyToOne
+    @JoinColumn(name = "evento_salud_id", referencedColumnName = "id")
+    private EventoSalud eventoSalud;
+
+    @ManyToOne
+    @JoinColumn(name = "horario_id", referencedColumnName = "id")
+    private Horario horario;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id", nullable = false)
+    private Usuario usuario;
+}
