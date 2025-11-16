c8887852b9eabe6f9a996dbceaaa8823ec15fdf6
Solus <gilgamesh06lg@gmail.com>
2025-11-16 14:06:08 -0500

    feat: Se creo el Service AuthService el cual tiene los metodos para register y login.

---
+++ Backend/PetHealthTracker/src/main/java/com/NoCountry/PetHealthTracker/auth/service/AuthService.java
@@ -0,0 +1,75 @@
+package com.NoCountry.PetHealthTracker.auth.service;
+
+import com.NoCountry.PetHealthTracker.auth.dto.LoginDTO;
+import com.NoCountry.PetHealthTracker.auth.dto.RegisterDTO;
+import com.NoCountry.PetHealthTracker.auth.dto.TokenDTO;
+import com.NoCountry.PetHealthTracker.model.entity.Persona;
+import com.NoCountry.PetHealthTracker.model.entity.RefreshToken;
+import com.NoCountry.PetHealthTracker.model.entity.Usuario;
+import com.NoCountry.PetHealthTracker.service.PersonaService;
+import com.NoCountry.PetHealthTracker.service.UsuarioService;
+import io.jsonwebtoken.Claims;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class AuthService {
+
+    private final PersonaService personaService;
+    private final UsuarioService usuarioService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final RefreshTokenService refreshTokenService;
+
+    public AuthService(PersonaService personaService,
+                       UsuarioService usuarioService,
+                       AuthenticationManager authenticationManager,
+                       JwtService jwtService, RefreshTokenService refreshTokenService){
+        this.personaService = personaService;
+        this.usuarioService = usuarioService;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+        this.refreshTokenService = refreshTokenService;
+    }
+
+    /**
+     * Metodo para registar el usarioo
+     * @param register DTO que contiene la informacion para registar al usaurio
+     * @return retorna un Usuario (temploral -> cambiar a DTO )
+     */
+    public Usuario registerUser(RegisterDTO register){
+        Persona persona = personaService.savePersona(register);
+        return usuarioService.saveUsuario(register, persona);
+    }
+
+
+    /**
+     * Metodo para realizar login
+     * @param login DTO que contiene el email y la password del usuario
+     * @return tokenDTO que contiene el token de acceso y el de refresco
+     */
+    public TokenDTO loginUser(LoginDTO login){
+
+        // Aquí entra el AuthenticationProvider configurado
+        Authentication auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        login.getEmail(),
+                        login.getPassword()
+                )
+        );
+        // Si llega aquí, significa que fue autenticado con éxito
+        UserDetails userDetails = (UserDetails) auth.getPrincipal();
+
+        // Generar el token con el usuario autenticado
+        String newAccessToken = jwtService.generateAccessToken(userDetails);
+        String newRefreshToken = jwtService.generateRefreshToken(userDetails);
+        Date expiration = jwtService.extractClaim(newRefreshToken, Claims::getExpiration);
+        RefreshToken RefreshToken = refreshTokenService.createRefreshToken(userDetails.getUsername(), newRefreshToken, expiration);
+        return new TokenDTO(newAccessToken,newRefreshToken);
+    }
+}
