67dae57b137c4e18fdbeeac7ecefc87c71c01c32
Solus <gilgamesh06lg@gmail.com>
2025-11-21 17:07:51 -0500

    feat: DTO para registrar un nuevo medicamento.

---
+++ Backend/PetHealthTracker/src/main/java/com/NoCountry/PetHealthTracker/model/dto/RegisterMedicamentoDTO.java
@@ -0,0 +1,30 @@
+package com.NoCountry.PetHealthTracker.model.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterMedicamentoDTO {
+
+    @NotBlank(message = "El nombre no puede ser vacio.")
+    private String nombre;
+
+    @NotBlank(message = "El tipo no puede ser vacio.")
+    private String tipo;
+
+    @NotBlank(message = "El fabricante no puede ser vacio.")
+    private String fabricante;
+
+    @NotNull(message = "El intervalo de dosis no puede ser nulo.")
+    @Positive(message = "El intervalo de dosis debe ser positivo")
+    private Integer intervaloDosis;
+
+    @NotBlank(message = "El nombre no puede ser vacio.")
+    private String descripcion;
+}
