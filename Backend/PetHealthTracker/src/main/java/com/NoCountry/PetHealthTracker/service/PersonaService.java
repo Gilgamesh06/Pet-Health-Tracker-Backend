9f2b1a5ad72e24e5b4fcdd280ecdaa2869824df6
Solus <gilgamesh06lg@gmail.com>
2025-11-20 21:45:29 -0500

    feat: Se agregaron los metodo para actualizar y eliminar.

--- Backend/PetHealthTracker/src/main/java/com/NoCountry/PetHealthTracker/service/PersonaService.java
+++ Backend/PetHealthTracker/src/main/java/com/NoCountry/PetHealthTracker/service/PersonaService.java
@@ -1,22 +1,36 @@
 package com.NoCountry.PetHealthTracker.service;
 
 import com.NoCountry.PetHealthTracker.auth.dto.RegisterDTO;
+import com.NoCountry.PetHealthTracker.model.dto.MessageDTO;
+import com.NoCountry.PetHealthTracker.model.dto.UpdatePersonaDTO;
+import com.NoCountry.PetHealthTracker.model.dto.UserInfoDTO;
 import com.NoCountry.PetHealthTracker.model.entity.Persona;
+import com.NoCountry.PetHealthTracker.model.entity.Usuario;
 import com.NoCountry.PetHealthTracker.repository.PersonaRepository;
-import com.NoCountry.PetHealthTracker.repository.UsuarioRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
 
 @Service
 public class PersonaService {
 
     private final PersonaRepository personaRepository;
+    private final UsuarioService usuarioService;
 
     public PersonaService(PersonaRepository personaRepository,
-                          UsuarioRepository usuarioRepository){
+                          UsuarioService usuarioService) {
         this.personaRepository = personaRepository;
+        this.usuarioService = usuarioService;
     }
 
-    protected Persona createObjectPersona(RegisterDTO register){
+    /**
+     * Metodo que crea un Objeto Persona a partir del DTO RegisterDTO
+     * @param register DTO -> Contiene los datos de registro
+     * @return Persona
+     */
+    protected Persona createObjectPersona(RegisterDTO register) {
         return Persona.builder()
                 .nombre(register.getNombre())
                 .apellido(register.getApellido())
@@ -24,8 +38,138 @@ public class PersonaService {
                 .build();
     }
 
-    public Persona savePersona(RegisterDTO register){
-        Persona persona =  createObjectPersona(register);
+    /**
+     * Metodo que guarda una Persona en la DB
+     * @param register DTO
+     * @return Persona  -> guardada
+     */
+    public Persona savePersona(RegisterDTO register) {
+        Persona persona = createObjectPersona(register);
         return personaRepository.save(persona);
     }
+
+    /**
+     * Metodo que calcula la edad
+     * @param fechaNacimiento
+     * @return Long con la edad
+     */
+    public Long calculetedAge(LocalDate fechaNacimiento){
+        LocalDate fechaActual = LocalDate.now();
+        // Calcula el periodo de tiempo en años entre la fecha actual (sistema) y la del usuario fechaNacimiento
+        int edad = Period.between(fechaNacimiento, fechaActual).getYears();
+        return (long) edad;
+    }
+
+
+    /**
+     * Metodo que Crea un DTO -> UserInfoDTO
+     * @param persona
+     * @param email
+     * @return UserInfoDTO
+     */
+    private UserInfoDTO createUserInfo(Persona persona, String email){
+        return UserInfoDTO.builder()
+                .nombre(persona.getNombre())
+                .apellido(persona.getApellido())
+                .edad(calculetedAge(persona.getFechaNacimiento()))
+                .email(email)
+                .build();
+    }
+
+    /**
+     * Metod que valida si el String no es nulo y no esta vacio
+     * @param value
+     * @return boolean
+     */
+    private boolean isNotEmpty(String value) {
+        return value != null && !value.trim().isEmpty();
+    }
+
+    /**
+     * Metodo que actualiza los datos de Perona
+     * @param persona
+     * @param updatePersona DTO
+     * @return Persona -> pueda haber sido actualizada o no
+     */
+    private Persona updatePerson(Persona persona, UpdatePersonaDTO updatePersona) {
+        // verifica si se ha hecho alguna actualización
+        boolean updated = false;
+        LocalDateTime now = LocalDateTime.now();
+
+        if (isNotEmpty(updatePersona.getNombre())) {
+            persona.setNombre(updatePersona.getNombre());
+            updated = true;
+        }
+        if (isNotEmpty(updatePersona.getApellido())) {
+            persona.setApellido(updatePersona.getApellido());
+            updated = true;
+        }
+        if (updatePersona.getFechaNacimiento() != null) {
+            persona.setFechaNacimiento(updatePersona.getFechaNacimiento());
+            updated = true;
+        }
+        if (updated) {
+            persona.setFechaActualizacion(now); // Establece la fecha de actualización si hay cambios
+        }
+
+        return persona;
+    }
+
+    /**
+     * Metodo que actualiza una persona
+     * @param updatePersona DTO
+     * @return UserInfoDTO
+     */
+    public UserInfoDTO updatePersona(UpdatePersonaDTO updatePersona){
+
+        // Obtiene el usuario del contexto
+        Usuario user = usuarioService.getUserAuthenticated();
+
+        // Actualiza los valores
+        Persona person = updatePerson(user.getPersona(), updatePersona);
+
+         Persona persona = personaRepository.save(person);
+
+        return createUserInfo(persona, user.getEmail());
+
+    }
+
+    /**
+     * Metodo que retorna los datos del user
+     * @return UserInfoDTO
+     */
+    public UserInfoDTO getUser(){
+        // Obtiene el usuario del contexto
+        Usuario user = usuarioService.getUserAuthenticated();
+
+        // Obtiene la persona logeada
+        Persona persona = user.getPersona();
+        return createUserInfo(persona, user.getEmail());
+    }
+
+
+    /**
+     * Metodo para eliminar un usuario
+     * @return MessageDTO
+     */
+    public MessageDTO delete(){
+
+        String estado = "INACTIVO";
+        LocalDateTime now = LocalDateTime.now();
+
+        // Obtiene el usuario del contexto
+        Usuario user = usuarioService.getUserAuthenticated();
+        user.setEstado(estado);
+        user.setFechaEliminacion(now);
+        Persona persona = user.getPersona();
+        persona.setEstado(estado);
+        persona.setFechaEliminacion(now);
+
+        return  MessageDTO.builder()
+                .message("Usuario:" +  persona.getNombre() + persona.getApellido() + " eliminado exitosamente.")
+                .build();
+    }
+
+
+
 }
--- Backend/PetHealthTracker/src/main/java/com/NoCountry/PetHealthTracker/service/UsuarioService.java
+++ Backend/PetHealthTracker/src/main/java/com/NoCountry/PetHealthTracker/service/UsuarioService.java
@@ -1,29 +1,79 @@
 package com.NoCountry.PetHealthTracker.service;
 
 import com.NoCountry.PetHealthTracker.auth.dto.RegisterDTO;
+import com.NoCountry.PetHealthTracker.auth.service.RefreshTokenService;
+import com.NoCountry.PetHealthTracker.model.dto.MessageDTO;
+import com.NoCountry.PetHealthTracker.model.dto.UpdateEmailDTO;
+import com.NoCountry.PetHealthTracker.model.dto.UpdatePasswordDTO;
 import com.NoCountry.PetHealthTracker.model.entity.Persona;
 import com.NoCountry.PetHealthTracker.model.entity.Usuario;
 import com.NoCountry.PetHealthTracker.repository.UsuarioRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+    private final RefreshTokenService refreshTokenService;
     private final PasswordEncoder passwordEncoder;
 
     public UsuarioService(UsuarioRepository usuarioRepository,
-                          PersonaService personaService,
+                          RefreshTokenService refreshTokenService,
                           PasswordEncoder passwordEncoder){
         this.usuarioRepository = usuarioRepository;
+        this.refreshTokenService = refreshTokenService;
         this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     * Metodo que busca un usuario a partir de su Email
+     * @param email correo del usuario
+     * @return Optional
+     */
+    public Optional<Usuario> getUserByEmail(String email){
+        return usuarioRepository.findByEmail(email);
+    }
 
-    protected Usuario createObjectUsario(RegisterDTO register, Persona persona){
+    /**
+     * Metodo que retorna el Email del Usuario Autenticado
+     * @return String -> Email
+     */
+    private String getEmailUserLogin(){
+        // 1. Obtener autenticación del contexto
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // 2. Obtener el email (UserDetails  atributo -> username = email )
+        return authentication.getName();
+    }
+
+    /**
+     * Metodo que retorna el Usuario autenticado // Crear excepcion personalizada y manaejarla Controller
+     * @return Usuario
+     */
+    protected Usuario getUserAuthenticated(){
+
+        String email = getEmailUserLogin();
+
+        Optional<Usuario> usuarioOpt = getUserByEmail(email);
+        if(usuarioOpt.isEmpty()){
+            throw new RuntimeException("Usuario no encontrado");
+        }
+        return usuarioOpt.get();
+    }
+
+    /**
+     * Metodo que crea un Objeto de tipo usuario a partir de DTO y Persona
+     * @param register DTO -> Email and Password
+     * @param persona Object -> relacion uno a uno
+     * @return Usuario
+     */
+    protected Usuario createObjectUsuario(RegisterDTO register, Persona persona){
         return Usuario.builder()
                 .email(register.getEmail())
                 .password(passwordEncoder.encode(register.getPassword()))
@@ -31,14 +81,80 @@ public class UsuarioService {
                 .build();
     }
 
+    /**
+     * Metodo que guarda el usaurio ingresado
+     * @param register DTO -> contiene el email y la password
+     * @param persona Objet -> Persona que esta vinculado al usuario
+     * @return Usuario objeto creado a partir del DTO y Persona
+     */
     public Usuario saveUsuario(RegisterDTO register, Persona persona){
 
-        Optional<Usuario> usuarioOpt = usuarioRepository.findByEmail(register.getEmail());
+        Optional<Usuario> usuarioOpt = getUserByEmail(register.getEmail());
         if(usuarioOpt.isEmpty()){
-            Usuario usuario = createObjectUsario(register, persona);
+            Usuario usuario = createObjectUsuario(register, persona);
             return usuarioRepository.save(usuario);
         }else{
             throw new RuntimeException("Usuario ya registrado");
         }
+    }
+
+
+    /**
+     * Metodo para cabiar la contraseña cuando esta logeado // nota crear excepcion personalizada y manejo
+     * @param updatePassword DTO -> contiene la nueva contraseña
+     * @return MessageDTO respuesta en formato JSON
+     */
+    public MessageDTO changePassword(UpdatePasswordDTO updatePassword){
+
+        // Obtiene el usuario del contexto
+        Usuario user = getUserAuthenticated();
+
+        if(user.getPassword().equals(updatePassword.getPassword())){
+            throw new RuntimeException("La contraseña no puede ser la misma.");
+        }
+        else{
+            // Codifica la contraseña
+            user.setPassword(passwordEncoder.encode(updatePassword.getPassword()));
+            user.setFechaActualizacion(LocalDateTime.now());
+            // Guarda la nueva contraseña
+            Usuario usuario =  usuarioRepository.save(user);
+
+            // Revoca los tokens de refresco
+            refreshTokenService.setRevocarToken(usuario);
+
+            return  MessageDTO.builder()
+                    .message("La contraseña fue modificada exitosamente.")
+                    .build();
+        }
+    }
+
+    /**
+     * Metodo para cambiar el Correo electronico del usaurio // Nota: Agregar excepcion personalizada y agregar manejo
+     * @param updateEmail DTO -> Contiene el nuevo correo
+     * @return MessageDTO respuesta en formato JSON
+     */
+    public MessageDTO changeEmail(UpdateEmailDTO updateEmail){
+
+        // Obtiene el usuario del contexto
+        Usuario user = getUserAuthenticated();
+
+        // verifica si el email es el mismo
+        if( user.getEmail().equals(updateEmail.getEmail())){
+            throw new RuntimeException("El correo no puede ser el mismo.");
+        }
+        else{
+            user.setEmail(updateEmail.getEmail());
+            user.setFechaActualizacion(LocalDateTime.now());
+            // Guarda la nueva contraseña
+            Usuario usuario =  usuarioRepository.save(user);
+
+            // Revoca los tokens de refresco
+            refreshTokenService.setRevocarToken(usuario);
+
+            return  MessageDTO.builder()
+                    .message("Email:" +  usuario.getEmail() + " modificado exitosamente.")
+                    .build();
+        }
+
     }
 }
