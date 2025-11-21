350f914a676f2783919c0e5699015ca6d7907dfc
Solus <gilgamesh06lg@gmail.com>
2025-11-20 21:50:52 -0500

    feat: Se agrego el metodo que busca por usuario y revocado

--- Backend/PetHealthTracker/src/main/java/com/NoCountry/PetHealthTracker/repository/RefreshTokenRepository.java
+++ Backend/PetHealthTracker/src/main/java/com/NoCountry/PetHealthTracker/repository/RefreshTokenRepository.java
@@ -1,13 +1,17 @@
 package com.NoCountry.PetHealthTracker.repository;
 
 import com.NoCountry.PetHealthTracker.model.entity.RefreshToken;
+import com.NoCountry.PetHealthTracker.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
     Optional<RefreshToken> findByToken(String token);
+
+    List<RefreshToken> findAllByUsuarioAndRevocado(Usuario usuario, Boolean condition);
 }
